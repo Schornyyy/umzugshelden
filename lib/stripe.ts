3e1daebf5f0668579e00091840b80381bb7f1c6b
schornyy <schornstein.lukas2001@outlook.de>
2025-11-05 20:25:00 +0100

    FIX: Mediathek & City pagination fix

--- lib/stripe.ts
+++
@@ -1,6 +0,0 @@
-import { loadStripe } from '@stripe/stripe-js';
-
-// Stripe Publishable Key aus Umgebungsvariablen
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
-
-export default stripePromise;
